5b1726795dc22044daa67912f3101a469a957298
kp <107309589+theexpensiveinformatics@users.noreply.github.com>
2024-06-02 18:35:12 +0530

    updated

--- lib/features/home/appBarWidgets/actions.dart
+++ lib/features/home/appBarWidgets/actions.dart
@@ -1,10 +1,12 @@
+import 'package:assignment_internshala/utils/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
-class AppBarActions extends StatelessWidget{
-
-  final double opacityBackBackground ;
+class AppBarActions extends StatelessWidget {
+  final double opacityBackBackground;
 
   const AppBarActions({super.key, required this.opacityBackBackground});
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,12 +21,43 @@ class AppBarActions extends StatelessWidget{
           color: Colors.white,
         ),
         onPressed: () {
-          /// -- bottom sheet
-
+          showModalBottomSheet(
+            enableDrag: true,
+            showDragHandle: true,
+            context: context,
+            builder: (BuildContext context) {
+              return SizedBox(
+                height: 200,
+                child: Column(
+                  children: [
+                    ListTile(
+                      leading: const Icon(Iconsax.link,size: AppSizes.iconMd,),
+                      title: Text('Invite',style: Theme.of(context).textTheme.titleLarge,),
+                      onTap: () {
+                        // Handle the invite action here
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Iconsax.user_add,size: AppSizes.iconMd,),
+                      title: Text('Add Member',style: Theme.of(context).textTheme.titleLarge,),
+                      onTap: () {
+                        // Handle the add member action here
+                      },
+                    ),
+                    ListTile(
+                      leading: const Icon(Iconsax.people,size: AppSizes.iconMd,),
+                      title: Text('Add Group',style: Theme.of(context).textTheme.titleLarge,),
+                      onTap: () {
+                        // Handle the add group action here
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
         },
       ),
     );
   }
-
-
 }
